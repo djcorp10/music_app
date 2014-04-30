967c4d0bee47d33d8f10743bdf78065eaafa3d66
David <djcorp10@smumn.edu>
2014-04-30 18:52:50 -0500

    migrate

---
+++ db/migrate/20140430222929_remove_timestamps_from_genre.rb
@@ -0,0 +1,12 @@
+class RemoveTimestampsFromGenre < ActiveRecord::Migration
+  def up
+    remove_column :genres, :created_at
+        remove_column :genres, :updated_at
+      end
+
+
+  def down
+    add_column :genres, :updated_at, :string
+    add_column :genres, :created_at, :string
+  end
+end
